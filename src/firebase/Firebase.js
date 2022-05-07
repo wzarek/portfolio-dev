104f52083d5a598a11d31d0e36fe3b3be6a039f5
PCWZAREK\wzarek <katanaforumps2@gmail.com>
2022-05-07 23:59:56 +0200

    Removed previous Firebase config

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,18 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage'
-//import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8mbFqEeYX7_XWSVslnr2A3O3FGcJdcSM",
-  authDomain: "portfolio-web-dev-bed65.firebaseapp.com",
-  projectId: "portfolio-web-dev-bed65",
-  storageBucket: "portfolio-web-dev-bed65.appspot.com",
-  messagingSenderId: "162329209572",
-  appId: "1:162329209572:web:7b7e79ac0bcf83ad939af1"
-};
-
-const firebaseConfig2 = {
   apiKey: "AIzaSyD8mbFqEeYX7_XWSVslnr2A3O3FGcJdcSM",
   authDomain: "portfolio-web-dev-bed65.firebaseapp.com",
   projectId: "portfolio-web-dev-bed65",
@@ -21,12 +11,8 @@ const firebaseConfig2 = {
   appId: "1:162329209572:web:56453f181a088d76939af1"
 };
 
-const app = initializeApp(firebaseConfig2);
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
-// const appCheck = initializeAppCheck(app, {
-//   provider: new ReCaptchaV3Provider('6LdmE8kfAAAAAGg4_ieOJIRyLy_Fm44vfWqRdtw3'),
-//   isTokenAutoRefreshEnabled: true
-// });
 
 export { storage, db, app };
